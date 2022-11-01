f162515180f5af0a8396fa650615ae69b3f98f72
Mohamed Elashri <muhammadelashri@gmail.com>
2022-11-01 15:38:17 -0400

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name        Twitter short-url remover
+// @namespace   twitter-t.co
+// @match       *://twitter.com/*
+// @match       *://*.twitter.com/*
+// @grant       none
+// @version     1.0
+// @author      Mohamed Elashri
+// @description Recovers the original link from twitter t.co urls
+// ==/UserScript==
+
+function getUrl(e) {
+	var target = e.target;
+	while ((target.tagName != "A" || !target.href) && target.parentNode) {
+		target = target.parentNode;
+	}
+	if (target.tagName != "A") return;
+	if (target.href.startsWith("https://t.co")) {
+		let url = '';
+		e.preventDefault();
+		url = e.target.textContent.replace("…","")
+		if(url.startsWith("http")) {
+			window.open(url, '_blank');
+		} else {
+			window.open(target.href, '_blank'); 
+		}
+	}
+}
+
+
+window.addEventListener("click", getUrl);
